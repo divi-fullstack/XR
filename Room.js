724bb1976b12f98c7ee11a4061b3cc6847a98e88
vinay.krishna <root@vinay-HP-Laptop-15s-gr0xxx.(none)>
2024-05-17 12:24:05 +0530

    update

---
+++ Room.js
@@ -0,0 +1,222 @@
+import * as THREE from 'three';
+
+
+function set_position(object, position){
+    object.position.x = position[0];
+    object.position.y = position[1];
+    object.position.z = position[2];
+}
+
+function set_rotation(object, rotation){
+    object.rotation.x = rotation[0];
+    object.rotation.y = rotation[1];
+    object.rotation.z = rotation[2];
+}
+
+function set_scale(object, scale_value){
+    object.scale.set(scale_value, scale_value, scale_value);
+}
+
+
+
+const DOOR_SIZE = 20;
+export class Wall {
+    constructor(width, height, position = [0,0,0], rotation = [0,0,0], material = null, windows = null){
+        this.object = new THREE.Object3D();
+        const geometry =  new THREE.BoxGeometry( width, height, 1);
+        const loader = new THREE.TextureLoader();
+        if (!material){
+            material = new THREE.MeshBasicMaterial( {map: loader.load('assets/wall-texture.jpeg'), side: THREE.DoubleSide} );
+        }
+        this.box = new THREE.Mesh(geometry, material);
+        this.object.add(this.box);
+        set_rotation(this.object, rotation);
+        set_position(this.object, position);
+    }
+    get_bounding_box(){
+        this.box.geometry.computeBoundingBox();
+        return this.box.geometry.boundingBox;
+    }
+}
+
+function recompute_all_bounding_boxes(room){
+    room.bounding_boxes = [];
+    for (var i=0; i<room.objects.length; i++)
+        room.bounding_boxes.push(room.objects[i].get_bounding_box());
+}
+
+export default class Room {
+    constructor(length, height, width){
+        this.bounding_boxes = []; //stores th bounding boxes of hard objects
+        this.objects = [];
+        this.player = (0,0);
+        this.length = length;
+        this.height = height;
+        this.width = width;
+        this.object = new THREE.Object3D();
+        const loader = new THREE.TextureLoader(); 
+
+        var wall_material = new THREE.MeshBasicMaterial( {map: loader.load('textures/bricks/roughness.jpg'), side: THREE.DoubleSide} );
+        var floor_material = new THREE.MeshBasicMaterial( {map: loader.load('assets/floor-texture.jpeg'), side: THREE.DoubleSide} );
+        var ceiling_material = new THREE.MeshBasicMaterial( {map: loader.load('assets/ceiling-texture.jpeg'), side: THREE.DoubleSide} );
+        var grass_material = new THREE.MeshBasicMaterial( {map: loader.load('assets/grass-texture.jpeg', function ( texture ) {
+            texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+            texture.offset.set(0, 0);
+            texture.repeat.set(20, 20)}), side: THREE.DoubleSide} 
+        );
+        this.walls = {
+            "Grass": {
+                "name" : "Grass",
+                "width": 200,
+                "height": 200,
+                "position": [0,-.5, 0],
+                "rotation": [Math.PI/2,0,0],
+                "material": grass_material,
+                "Wall": null
+            },
+            "Front": {
+                "name" : "Back",
+                "width": 40,
+                "height": 6,
+                "position": [0,3,-15],
+                "rotation": [0,0,0],
+                "material": wall_material,
+                "Wall": null
+            },
+            "Back": {
+                "name" : "Front",
+                "width": 40,
+                "height": 6,
+                "position": [0,3,15],
+                "rotation": [0,0,0],
+                "material": wall_material,
+                "Wall": null
+            }, 
+            "Left": {
+                "name" : "Left",
+                "width": 30-DOOR_SIZE/2,
+                "height": 6,
+                "position": [-20,3,-5],
+                "rotation": [0,Math.PI/2,0],
+                "material": wall_material,
+                "Wall": null
+            },
+            // "Middle": {
+            //     "name" : "Middle",
+            //     "width": length - DOOR_SIZE,
+            //     "height": height,
+            //     "position": [0,0,length/2],
+            //     "rotation": [0,Math.PI/2,0],
+            //     "material": wall_material,
+            //     "Wall": null
+            // },
+            "Right": {
+                "name" : "Right",
+                "width": 30,
+                "height": 6,
+                "position": [20,3,0],
+                "rotation": [0,Math.PI/2,0],
+                "material": wall_material,
+                "Wall": null
+            }, 
+            "Ground": {
+                "name" : "Ground",
+                "width": 40,
+                "height": 30,
+                "position": [0,-.45,0],
+                "rotation": [Math.PI/2,0,0],
+                "material": floor_material,
+                "Wall": null
+            }, 
+            "Ceiling": {
+                "name" : "Ceiling",
+                "width": 40,
+                "height": 30,
+                "position": [0,6,0],
+                "rotation": [Math.PI/2,0,0],
+                "material": ceiling_material,
+                "Wall": null
+            }
+        }; 
+        for (var wall_name in this.walls){
+            var wall = this.walls[wall_name];
+            var wall_object = new Wall(wall.width, wall.height, wall.position, wall.rotation, wall.material);
+            console.log(wall.position, wall.rotation, wall.material);
+            wall.Wall = wall_object;
+            this.objects.push(wall_object);
+            this.object.add(wall_object.object);
+        }
+        // this.decorations = {
+        //     "Philopoemen": {
+        //         "scale": 0.04,
+        //         "path": './objects/philopoemen/scene.gltf',
+        //         "position": [5, -8, 60],
+        //         "rotation": [Math.PI/2, -Math.PI, Math.PI/2]
+        //     },
+        //     "Neptune": {
+        //         "scale": 1,
+        //         "path": './objects/neptune/scene.gltf',
+        //         "position": [25, -10, 9],
+        //         "rotation": [-Math.PI/2, 0, Math.PI/2]
+        //     },
+        //     "Bench": {
+        //         "scale": 0.08,
+        //         "path": './objects/bench/scene.gltf',
+        //         "position": [-15, -10, 40],
+        //         "rotation": [Math.PI/2,Math.PI,Math.PI/2]
+        //     },
+        //     "Bonsai": {
+        //         "scale": 0.1,
+        //         "path": './objects/bonsai/scene.gltf',
+        //         "position": [-25,-10,0],
+        //         "rotation": [Math.PI/2,Math.PI,0]
+        //     },
+        //     "Light": {
+        //         "scale": 0.008,
+        //         "path": './objects/light/scene.gltf',
+        //         "position": [-15,3.6,43],
+        //         "rotation": [Math.PI/2,Math.PI,0]
+        //     }
+        // };
+        // for (var decoration_name in this.decorations){
+        //     var decoration = this.decorations[decoration_name];
+        //     var main_object = load_object(decoration);
+        //     // decoration.tmp = main_object;
+        //     this.object.add(main_object);
+        // }
+        recompute_all_bounding_boxes(this);
+    }
+    
+    inside_solid(vec){
+        for (var i = 0; i < this.bounding_boxes.length; i++){
+            var box = this.bounding_boxes[i];
+            if (box.containsPoint(vec)){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    ray_intersects(ray){
+        for (var i = 0; i < this.bounding_boxes.length; i++){
+            
+            var box = this.bounding_boxes[i];
+            if (ray.intersectsBox(box)){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    triangle_intersects(triangle){
+        for (var i = 0; i < this.bounding_boxes.length; i++){
+            console.log("Checking box " + String(i));
+            var box = this.bounding_boxes[i];
+            if (box.intersectsTriangle(triangle)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
--- ttbs.js
+++ ttbs.js
@@ -6,39 +6,55 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
 import { CCDIKSolver, CCDIKHelper } from 'three/examples/jsm/animation/CCDIKSolver.js';
 import Stats from 'three/addons/libs/stats.module.js';
-import { GUI } from 'three/addons/libs/lil-gui.module.min.js'; 
-import { XRButton } from 'three/addons/webxr/XRButton.js'; 
-let scene, camera, renderer, orbitControls, transformControls;
+import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
+import { XRButton } from 'three/addons/webxr/XRButton.js';
+import gsap from 'gsap';
+import Room from "./Room.js"
+import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
+import { FontLoader } from 'three/addons/loaders/FontLoader.js';
+let scene, camera, renderer, orbitControls, transformControls, room;
 let mirrorSphereCamera;
 let controller1, controller2;
 let controllerGrip1, controllerGrip2, ambientLight;
 
-let raycaster, gltf, group,switchMaterial, switchMesh;
+let raycaster, gltf, group, switchMaterial, switchMesh;
 
 const intersected = [];
 
 const OOI = {};
 let IKSolver;
-
-let stats,gui, conf;
+const ROOM_WIDTH = 40;
+const ROOM_HEIGHT = 20;
+const ROOM_LENGTH = 30;
+let stats, gui, conf;
 const v0 = new THREE.Vector3();
 
-init(animate); 
+init(animate);
 async function init(animate) {
 
-  
-    scene = new THREE.Scene();
-    scene.fog = new THREE.FogExp2(0x808080, .17);
-    scene.background = new THREE.Color(0xffffff);
 
+    scene = new THREE.Scene();
+    // scene.fog = new THREE.FogExp2(0x808080, .17);
+    // scene.background = new THREE.Color(0xffffff);
+    scene.background = new THREE.Color('#c8f0f9')
+    console.log(scene, 'dddd')
     camera = new THREE.PerspectiveCamera(55, window.innerWidth / window.innerHeight, 0.001, 5000);
-    // camera.position.set(0, 1.6, 0);
-    camera.position.set(5.21, 1.6, 3.97);
+    camera.position.set(10, 4, 40);
+    // camera.position.set(5.21, 1.6, 3.97);
+    scene.rotation.x=0
+    scene.rotation.y=1.56
+    scene.rotation.z=0
 
     camera.lookAt(scene.position);
 
     ambientLight = new THREE.AmbientLight(0xffffff, 5); // soft white light
     // scene.add(ambientLight)
+
+    room = new Room(ROOM_LENGTH, ROOM_HEIGHT, ROOM_WIDTH);
+    scene.add(room.object);  
+    const sunLight = new THREE.DirectionalLight(0xc8f0f9, 1.96)
+    sunLight.position.set(-69, 44, 14)
+    scene.add(sunLight)
     renderer = new THREE.WebGLRenderer({ antialias: true, logarithmicDepthBuffer: true });
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
@@ -47,10 +63,10 @@ async function init(animate) {
     // stats = new Stats();
     // document.body.appendChild(stats.dom);
 
-    // orbitControls = new OrbitControls(camera, renderer.domElement);
+    orbitControls = new OrbitControls(camera, renderer.domElement);
     // orbitControls.minDistance = 0.2;
-    // orbitControls.maxDistance = 1.5;
-    // orbitControls.enableDamping = true;
+    orbitControls.maxDistance = 100;
+    orbitControls.enableDamping = true;
 
     const dracoLoader = new DRACOLoader();
     dracoLoader.setDecoderPath('/draco/');
@@ -58,53 +74,53 @@ async function init(animate) {
     gltfLoader.setDRACOLoader(dracoLoader);
 
     // const gltf = await gltfLoader.loadAsync('/models/kira.glb' );
-    const gltff = await gltfLoader.loadAsync('/models/output.glb' );
+    const gltff = await gltfLoader.loadAsync('/models/output.glb');
 
-
-    console.log(gltff,'l')
+    console.log(gltff, 'gltff')
     group = new THREE.Group();
-    scene.add( group );
+    scene.add(group);
 
 
     var loader1 = new THREE.TextureLoader();
 
-// Load an image file into a custom material
-var material1 = new THREE.MeshLambertMaterial({
-  map: loader1.load('/models/image.jpeg')
-});
+    // Load an image file into a custom material
+    var material1 = new THREE.MeshLambertMaterial({
+        map: loader1.load('/models/image.jpeg')
+    });
 
-// create a plane geometry for the image with a width of 10
-// and a height that preserves the image's aspect ratio
-var geometry1 = new THREE.PlaneGeometry(1.5, 2.5);
+    // create a plane geometry for the image with a width of 10
+    // and a height that preserves the image's aspect ratio
+    var geometry1 = new THREE.PlaneGeometry(1.5, 2.5);
 
-// combine our image geometry and material into a mesh
-var mesh1 = new THREE.Mesh(geometry1, material1);
+    // combine our image geometry and material into a mesh
+    var mesh1 = new THREE.Mesh(geometry1, material1);
 
-// set the position of the image mesh in the x,y,z dimensions
-mesh1.position.set(-1,3,3)
-mesh1.rotation.y = 2
-// add the image to the scene
-scene.add(mesh1);   
+    // set the position of the image mesh in the x,y,z dimensions
+    mesh1.position.set(-1, 3, 3)
+    mesh1.rotation.y = 2
+    // add the image to the scene
+    scene.add(mesh1);
+
 
 
     const geometries = [
-        new THREE.BoxGeometry( 0.2, 0.2, 0.2 ),
-        new THREE.ConeGeometry( 0.2, 0.2, 64 ),
-        new THREE.CylinderGeometry( 0.2, 0.2, 0.2, 64 ),
-        new THREE.IcosahedronGeometry( 0.2, 8 ),
-        new THREE.TorusGeometry( 0.2, 0.04, 64, 32 ),
+        new THREE.BoxGeometry(0.2, 0.2, 0.2),
+        new THREE.ConeGeometry(0.2, 0.2, 64),
+        new THREE.CylinderGeometry(0.2, 0.2, 0.2, 64),
+        new THREE.IcosahedronGeometry(0.2, 8),
+        new THREE.TorusGeometry(0.2, 0.04, 64, 32),
     ];
 
-    for ( let i = 0; i < geometries.length; i ++ ) {
+    for (let i = 0; i < geometries.length; i++) {
 
-        const geometry = geometries[ i ];
-        const material = new THREE.MeshStandardMaterial( {
+        const geometry = geometries[i];
+        const material = new THREE.MeshStandardMaterial({
             color: Math.random() * 0xffffff,
             roughness: 0.7,
             metalness: 0.0
-        } );
+        });
 
-        const object = new THREE.Mesh( geometry, material );
+        const object = new THREE.Mesh(geometry, material);
 
         object.position.x = Math.random() * 4 - 2;
         object.position.y = Math.random() * 2;
@@ -114,16 +130,16 @@ scene.add(mesh1);
         object.rotation.y = Math.random() * 2 * Math.PI;
         object.rotation.z = Math.random() * 2 * Math.PI;
 
-        object.scale.setScalar( Math.random() + 0.5 );
+        object.scale.setScalar(Math.random() + 0.5);
 
         object.castShadow = true;
         object.receiveShadow = true;
 
-        group.add( object );
+        group.add(object);
 
     }
     // gltf.scene.traverse(n => {
- 
+
     //     if (n.name === 'head') OOI.head = n;
     //     if (n.name === 'lowerarm_l') OOI.lowerarm_l = n;
     //     if (n.name === 'Upperarm_l') OOI.Upperarm_l = n;
@@ -144,39 +160,66 @@ scene.add(mesh1);
     // }
 
 
-    const switchGeometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+    const loaderf = new FontLoader(); 
+loaderf.load( 'fonts/gentilis_bold.typeface.json', function ( font ) {
+
+ const wall_material_text = new TextGeometry( 'techindo systems pvt ltd', {
+    font: font,
+    size: .9,
+    depth: 0.2,
+   
+} );
+const material = new THREE.MeshStandardMaterial({
+    color: 0x0097DD0,
+    roughness: 0.7,
+    metalness: 0.0
+});
+
+const object = new THREE.Mesh(wall_material_text, material);
+    object.position.set(-20.5,4,-10)
+    object.rotation.x=0
+    object.rotation.y=4.7
+    object.rotation.z=0
+
+scene.add(object)
+} );
+
+
+
+
+    const switchGeometry = new THREE.BoxGeometry(1, 1, 0.2);
     switchMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    switchMesh = new THREE.Mesh(switchGeometry, switchMaterial); 
-    switchMesh.position.x = 1;
-    switchMesh.position.y = 1.5;
-    switchMesh.position.z = 3;
-    switchMesh.name='lightSwitch'
+    switchMesh = new THREE.Mesh(switchGeometry, switchMaterial);
+    switchMesh.position.x = -15;
+    switchMesh.position.y = 3;
+    switchMesh.position.z = 14.5;
+    switchMesh.name = 'lightSwitch'
     group.add(switchMesh);
 
-   
+
 
     // Toggle switch state
-  
+
 
     const mouse = new THREE.Vector2();
 
 
-window.addEventListener('click', (event) => {
-    // Calculate mouse position in normalized device coordinates
-    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+    window.addEventListener('click', (event) => {
+        // Calculate mouse position in normalized device coordinates
+        mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+        mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
 
-    // Update the picking ray with the camera and mouse position
-    raycaster.setFromCamera(mouse, camera);
+        // Update the picking ray with the camera and mouse position
+        raycaster.setFromCamera(mouse, camera);
 
-    // Calculate objects intersecting the picking ray
-    const intersects = raycaster.intersectObjects(scene.children);
+        // Calculate objects intersecting the picking ray
+        const intersects = raycaster.intersectObjects(scene.children);
 
-    // Check if the switch was clicked
-    if (intersects.length > 0 && intersects[0].object === switchMesh) {
-        toggleSwitch();
-    }
-});
+        // Check if the switch was clicked
+        if (intersects.length > 0 && intersects[0].object === switchMesh) {
+            toggleSwitch();
+        }
+    });
 
 
     // orbitControls.target.copy(OOI.sphere.position); // orbit controls lookAt the sphere
@@ -222,49 +265,49 @@ window.addEventListener('click', (event) => {
     ];
     // IKSolver = new CCDIKSolver(OOI.kira, iks);  
 
-     // Setup XR controllers
-     controller1 = renderer.xr.getController( 0 );
-     controller1.addEventListener( 'selectstart', onSelectStart );
-     controller1.addEventListener( 'selectend', onSelectEnd );
-     controller1.addEventListener( 'select', selectHandler );
+    // Setup XR controllers
+    controller1 = renderer.xr.getController(0);
+    controller1.addEventListener('selectstart', onSelectStart);
+    controller1.addEventListener('selectend', onSelectEnd);
+    controller1.addEventListener('select', selectHandler);
     //  controller1.addEventListener( 'squeezeend', onSelectEnd1 );
-     scene.add( controller1 );
- 
-     controller2 = renderer.xr.getController( 1 );
-     controller2.addEventListener( 'selectstart', onSelectStart );
-     controller2.addEventListener( 'selectend', onSelectEnd );
-     controller2.addEventListener( 'select', selectHandler );
-    //  controller2.addEventListener( 'squeezeend', onSelectEnd1 );
-     scene.add( controller2 );
- 
-     const controllerModelFactory = new XRControllerModelFactory();
- 
-     controllerGrip1 = renderer.xr.getControllerGrip( 0 );
-     controllerGrip1.add( controllerModelFactory.createControllerModel( controllerGrip1 ) );
-     scene.add( controllerGrip1 );
- 
-     controllerGrip2 = renderer.xr.getControllerGrip( 1 );
-     controllerGrip2.add( controllerModelFactory.createControllerModel( controllerGrip2 ) );
-     scene.add( controllerGrip2 );
- 
-     //
- 
-     const geometry = new THREE.BufferGeometry().setFromPoints( [ new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, 0, - 1 ) ] );
- 
-     const line = new THREE.Line( geometry );
-     line.name = 'line';
-     line.scale.z = 5;
- 
-     controller1.add( line.clone() );
-     controller2.add( line.clone() );
- 
-     raycaster = new THREE.Raycaster();
+    scene.add(controller1);
 
-     
+    controller2 = renderer.xr.getController(1);
+    controller2.addEventListener('selectstart', onSelectStart);
+    controller2.addEventListener('selectend', onSelectEnd);
+    controller2.addEventListener('select', selectHandler);
+    //  controller2.addEventListener( 'squeezeend', onSelectEnd1 );
+    scene.add(controller2);
+
+    const controllerModelFactory = new XRControllerModelFactory();
+
+    controllerGrip1 = renderer.xr.getControllerGrip(0);
+    controllerGrip1.add(controllerModelFactory.createControllerModel(controllerGrip1));
+    scene.add(controllerGrip1);
+
+    controllerGrip2 = renderer.xr.getControllerGrip(1);
+    controllerGrip2.add(controllerModelFactory.createControllerModel(controllerGrip2));
+    scene.add(controllerGrip2);
+
+    //
+
+    const geometry = new THREE.BufferGeometry().setFromPoints([new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 0, - 1)]);
+
+    const line = new THREE.Line(geometry);
+    line.name = 'line';
+    line.scale.z = 5;
+
+    controller1.add(line.clone());
+    controller2.add(line.clone());
+
+    raycaster = new THREE.Raycaster();
+
+
 
     window.addEventListener('resize', onWindowResize, false);
     animate()
-} 
+}
 
 function updateIK() {
 
@@ -286,31 +329,34 @@ function onWindowResize() {
     renderer.setSize(window.innerWidth, window.innerHeight);
 
 }
-renderer.xr.enabled = true;
+if (renderer?.xr) {
+    renderer.xr.enabled = true;
+
+}
 
 // document.body.appendChild( VRButton.createButton( renderer ) );
-document.body.appendChild( XRButton.createButton( renderer, { 'optionalFeatures': [ 'depth-sensing'] } ));
+document.body.appendChild(XRButton.createButton(renderer, { 'optionalFeatures': ['depth-sensing'] }));
 
 
 
 
-function onSelectStart( event ) {
+function onSelectStart(event) {
     // scene.add(ambientLight);
 
-    const controller = event.target; 
-    console.log(controller,'ffffevenrt')
-    const intersections = getIntersections( controller );
+    const controller = event.target;
+    console.log(controller, 'ffffevenrt')
+    const intersections = getIntersections(controller);
 
-    if ( intersections.length > 0 ) {
+    if (intersections.length > 0) {
 
-        const intersection = intersections[ 0 ];
+        const intersection = intersections[0];
 
         const object = intersection.object;
-        if(object == switchMesh){
+        if (object == switchMesh) {
             return
         }
         object.material.emissive.b = 1;
-        controller.attach( object );
+        controller.attach(object);
         controller.userData.selected = object;
 
     }
@@ -319,28 +365,28 @@ function onSelectStart( event ) {
 
 }
 
-let isSwitchOn = false; 
+let isSwitchOn = false;
 function toggleSwitch() {
     isSwitchOn = !isSwitchOn;
-    if(isSwitchOn){
+    if (isSwitchOn) {
         scene.add(ambientLight);
-    }else{
+    } else {
         scene.remove(ambientLight);
     }
     switchMaterial.color.set(!isSwitchOn ? 0x00ff00 : 0xff0000); // Green if on, red if off
 }
 
 
-function selectHandler( event ) {
-    
-    const controller = event.target; 
-    console.log(controller,'ffffevenrt')
-    const intersections = getIntersections( controller );
+function selectHandler(event) {
 
-    if ( intersections.length > 0 ) {
+    const controller = event.target;
+    console.log(controller, 'ffffevenrt')
+    const intersections = getIntersections(controller);
 
-        const intersection = intersections[ 0 ];
-        if(intersection.object == switchMesh){
+    if (intersections.length > 0) {
+
+        const intersection = intersections[0];
+        if (intersection.object == switchMesh) {
             toggleSwitch()
         }
 
@@ -348,27 +394,27 @@ function selectHandler( event ) {
 
 }
 
-function onSelectEnd1( event ) {
+function onSelectEnd1(event) {
     scene.remove(ambientLight);
- 
+
 
 }
 
 
 
-function onSelectEnd( event ) {
+function onSelectEnd(event) {
     // scene.remove(ambientLight);
 
     const controller = event.target;
 
-    if ( controller.userData.selected !== undefined ) {
+    if (controller.userData.selected !== undefined) {
 
         const object = controller.userData.selected;
-        if(object == switchMesh){
+        if (object == switchMesh) {
             return
         }
         object.material.emissive.b = 0;
-        group.attach( object );
+        group.attach(object);
 
         controller.userData.selected = undefined;
 
@@ -376,36 +422,36 @@ function onSelectEnd( event ) {
 
 }
 
-function getIntersections( controller ) {
+function getIntersections(controller) {
 
     controller.updateMatrixWorld();
 
-    raycaster.setFromXRController( controller );
+    raycaster.setFromXRController(controller);
 
-    return raycaster.intersectObjects( group.children, false );
+    return raycaster.intersectObjects(group.children, false);
 
 }
 
-function intersectObjects( controller ) {
+function intersectObjects(controller) {
 
     // Do not highlight in mobile-ar
 
-    if ( controller.userData.targetRayMode === 'screen' ) return;
+    if (controller.userData.targetRayMode === 'screen') return;
 
     // Do not highlight when already selected
 
-    if ( controller.userData.selected !== undefined ) return;
+    if (controller.userData.selected !== undefined) return;
 
-    const line = controller.getObjectByName( 'line' );
-    const intersections = getIntersections( controller );
+    const line = controller.getObjectByName('line');
+    const intersections = getIntersections(controller);
 
-    if ( intersections.length > 0 ) {
+    if (intersections.length > 0) {
 
-        const intersection = intersections[ 0 ];
+        const intersection = intersections[0];
 
         const object = intersection.object;
         object.material.emissive.r = 1;
-        intersected.push( object );
+        intersected.push(object);
 
         line.scale.z = intersection.distance;
 
@@ -419,7 +465,7 @@ function intersectObjects( controller ) {
 
 function cleanIntersected() {
 
-    while ( intersected.length ) {
+    while (intersected.length) {
 
         const object = intersected.pop();
         object.material.emissive.r = 0;
@@ -430,49 +476,49 @@ function cleanIntersected() {
 
 
 
-function animate (){
-    renderer.setAnimationLoop( function () {
+function animate() {
+    renderer.setAnimationLoop(function () {
 
         // animate()
         // cleanIntersected();
-    
+
         // intersectObjects( controller1 );
         // intersectObjects( controller2 );
         // if (OOI.sphere && mirrorSphereCamera) {
-    
+
         //     OOI.sphere.visible = false;
         //     OOI.sphere.getWorldPosition(mirrorSphereCamera.position);
         //     mirrorSphereCamera.update(renderer, scene);
         //     OOI.sphere.visible = true;
-    
+
         // }
-    
+
         // if (OOI.sphere && conf.followSphere) {
-    
+
         //     // orbitControls follows the sphere
         //     OOI.sphere.getWorldPosition(v0);
         //     orbitControls.target.lerp(v0, 0.1);
-    
+
         // }
-    
+
         // if (OOI.head && OOI.sphere && conf.turnHead) {
-    
+
         //     // turn head
         //     OOI.sphere.getWorldPosition(v0);
         //     OOI.head.lookAt(v0); 
-    
+
         //     OOI.head.rotation.set(OOI.head.rotation.x, OOI.head.rotation.y + Math.PI, OOI.head.rotation.z);
-    
+
         // }
-    
+
         // if (conf.ik_solver) {
-    
+
         //     updateIK();
-    
+
         // }
-    
-        // orbitControls.update();
-        renderer.render(scene, camera); 
-    
-    } );
+
+        orbitControls.update();
+        renderer.render(scene, camera);
+
+    });
 }
